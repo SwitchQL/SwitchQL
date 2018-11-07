6a405fd0e1121d65c002a7a8a4e0c7a726b6800f
Zahaan <zahaanjasani@gmail.com>
2018-11-07 13:44:34 -0800

    finished logic, testing conversion

--- src/server/server.js
+++ src/server/server.js
@@ -4,12 +4,15 @@ const electron = require('electron');
 const { ipcMain } = electron;
 const dbController = require('./dbController.js');
 const logicController = require('./logicController');
+//const {parseGraphqlServer, toTitleCase, createFindAllRootQuery, buildGraphqlRootQuery, createSubQuery, buildGraphqlTypeSchema} = require('./conversionController');
 
 ipcMain.on('url', async (event, url) => {
   const dbMetaData = await dbController.getSchemaInfo(url);
   const formattedMetaData = await logicController.formatMetaData(dbMetaData);
-  console.log(formattedMetaData);
-  event.sender.send('testback', JSON.stringify(formattedMetaData));
+  const combo = { dbMetaData , formattedMetaData };
+  event.sender.send('testback', JSON.stringify(combo));
+  // const test = await parseGraphqlServer(formattedMetaData.tables);
+  // console.log(test);
 });
 
 // app.post('/getInfo', dbController.getSchemaInfo, (req, res) => {
