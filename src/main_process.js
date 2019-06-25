39d25980e63616472d9fd606bda6079a691f1d77
Edmund Hood Highcock <edmund@greenbyte.com>
2019-06-25 17:06:42 +0200

    Window width

--- src/main_process.js
+++ src/main_process.js
@@ -24,7 +24,7 @@ app.on("ready", function() {
   const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
   //create new window
   mainWindow = new BrowserWindow({
-    width: width ,
+    width: width / 2,
     height: height
   });
 
